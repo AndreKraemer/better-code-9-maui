bcfc228d92125cbe6c36f0a2ff431fa17e343b67
André Krämer <andre@andrekraemer.de>
2024-11-19 13:42:25 +0100

    FinishedHybridSample

---
+++ MiracleListVueHybridDemo/MiracleListVueHybridDemo/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace MiracleListVueHybridDemo
+{
+  public partial class MainPage : ContentPage
+  {
+
+
+    public MainPage()
+    {
+      InitializeComponent();
+    }
+
+    private void OnSendMessageButtonClicked(object sender, EventArgs e)
+    {
+      hybridWebView.SendRawMessage(messageText.Text);
+    }
+
+    private async void OnHybridWebViewRawMessageReceived(object sender, HybridWebViewRawMessageReceivedEventArgs e)
+    {
+      await DisplayAlert("Raw Message Received", e.Message, "OK");
+    }
+
+  }
+
+}
---
+++ MiracleListVueHybridDemo/MiracleListVueHybridDemo/Resources/Raw/wwwroot/precache-manifest.f7b13ee739702568b34ac23a79a53a28.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a235ee78898e2cecbf79",
+    "url": "/css/about.b4b117c2.css"
+  },
+  {
+    "revision": "e4144b27ffe4358234ea86d48c68b3af",
+    "url": "/css/bootstrap/bootstrap.min.css"
+  },
+  {
+    "revision": "740149b1344ae058ad90",
+    "url": "/css/chunk-vendors.d075f4ea.css"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/css/fonts/glyphicons-halflings-regular.eot"
+  },
+  {
+    "revision": "f721466883998665b87923b92dea655b",
+    "url": "/css/fonts/glyphicons-halflings-regular.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/css/fonts/glyphicons-halflings-regular.ttf"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/css/fonts/glyphicons-halflings-regular.woff"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/css/fonts/glyphicons-halflings-regular.woff2"
+  },
+  {
+    "revision": "bbb6a82b912e14e203f6",
+    "url": "/css/home.ba43eed0.css"
+  },
+  {
+    "revision": "31e237640d9d8d8dc3672df68e0cc41a",
+    "url": "/img/MiracleListLogo.jpg"
+  },
+  {
+    "revision": "63f12b82956b9b5bc5608ef5a9e55d72",
+    "url": "/img/MiracleListLogo.png"
+  },
+  {
+    "revision": "4308bcec99a231d9a34f28c7b92123a2",
+    "url": "/img/MiracleListLogo_ohneText.jpg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/VueLogo.png"
+  },
+  {
+    "revision": "14e07de613f23c2def46f9e31f895259",
+    "url": "/img/background.png"
+  },
+  {
+    "revision": "06066720ea834056e57e9c3238e8311a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a235ee78898e2cecbf79",
+    "url": "/js/about.8fd5e167.js"
+  },
+  {
+    "revision": "192c5ec03dbb3ff939db",
+    "url": "/js/app.a8e08407.js"
+  },
+  {
+    "revision": "105a4995b8777aeaf68bff64bf7d2ae0",
+    "url": "/js/bootstrap.min.js"
+  },
+  {
+    "revision": "740149b1344ae058ad90",
+    "url": "/js/chunk-vendors.b681f218.js"
+  },
+  {
+    "revision": "bbb6a82b912e14e203f6",
+    "url": "/js/home.9c6fa23a.js"
+  },
+  {
+    "revision": "f809a3bf23588ccde269d88003bdc31e",
+    "url": "/js/hybridwebview.js"
+  },
+  {
+    "revision": "e9a6d5f25b9de1216b81af8e9e19c24a",
+    "url": "/js/jquery.min.js"
+  },
+  {
+    "revision": "1d295e42c55ad162b5da305bb3678a80",
+    "url": "/js/signalr.min.js"
+  },
+  {
+    "revision": "f3c02d50befcd5f9b3b0578f39f015dc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a918c3f1b2ef9b8c9d744f2f2877b9a9",
+    "url": "/styles.css"
+  },
+  {
+    "revision": "b971ea12b285d3fe59fcb8ebfa592f7d",
+    "url": "/web.config"
+  }
+]);
